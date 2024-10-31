ab002ff8b0a846bf8fb1ed5d4e73c54e60b92ab1
paiva2 <joaovitor.paiva145@hotmail.com>
2024-10-31 16:49:36 -0300

    [CHORE] initial configs

---
+++ src/main/java/org/com/wired/BackendChallengeWiredcraftApplication.java
@@ -0,0 +1,11 @@
+package org.com.wired;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendChallengeWiredcraftApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BackendChallengeWiredcraftApplication.class, args);
+    }
+}
